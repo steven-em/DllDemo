689b290fdbe57e5bb0ad92574f212a854a5162a2
zengxiuqin <1205277109@qq.com>
2018-01-04 17:02:51 +0800

    2017-6

---
+++ src/main/java/com/rljc/utils/GitUtils.java
@@ -0,0 +1,32 @@
+package com.rljc.utils;
+
+/**
+ * git 一些命令的帮助类
+ * @author Administrator
+ *
+ */
+public class GitUtils {
+	
+	/**
+	 * 分值
+	 */
+	public static void branch(){
+		
+	}
+	
+	/**
+	 * 合并
+	 */
+	public static void merge(){
+		
+	}
+	
+	/**
+	 * 检测
+	 */
+	public static void checkout(){
+		
+	}
+	
+
+}
